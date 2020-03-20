a872f9ed82a86dea97e3b6a10b2ec2238711a6e4
Débora Correia <debora@labcodes.com.br>
2020-03-20 17:00:03 -0300

    Rearrange directory structure to separate documentation from the design system

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,6 @@
 import { configure } from '@storybook/react';
 
-import '!style-loader!css-loader!sass-loader!../scss/main.scss';
+import '!style-loader!css-loader!sass-loader!../labsystem/scss/main.scss';
 
 // automatically import all files ending in *.stories.js
 configure(require.context('../stories', true, /\.stories\.(js|mdx)$/), module);
--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,6 +1,6 @@
 import { addons } from '@storybook/addons';
 import labcodesDesignSystem from './labcodesDesignSystem';
-import '!style-loader!css-loader!sass-loader!../theme/main.scss';
+import '!style-loader!css-loader!sass-loader!./theme/main.scss';
 
 addons.setConfig({
   theme: labcodesDesignSystem,
