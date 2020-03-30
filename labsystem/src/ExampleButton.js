5c5bdcd33ec51a508c2692dded04f2984d00d142
Luciano Ratamero <luciano@ratamero.com>
2020-03-30 12:34:35 -0300

    ESLint

--- labsystem/src/ExampleButton.js
+++ labsystem/src/ExampleButton.js
@@ -1,18 +1,28 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
 export default class ExampleButton extends React.Component {
   static propTypes = {
-    type: PropTypes.oneOf(['primary', 'secondary', 'info', 'danger', 'success'])
-  }
+    type: PropTypes.oneOf([
+      "primary",
+      "secondary",
+      "info",
+      "danger",
+      "success"
+    ]),
+    children: PropTypes.array
+  };
 
   static defaultProps = {
-    type: 'primary'
-  }
+    type: "primary",
+    children: []
+  };
 
   render() {
     return (
-      <button type="button" className={`button ${this.props.type}`}>{this.props.children}</button>
-    )
+      <button type="button" className={`button ${this.props.type}`}>
+        {this.props.children}
+      </button>
+    );
   }
 }
