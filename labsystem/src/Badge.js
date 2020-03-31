742e1cf941c918c63f1a708ee4b3a783ea398b89
Olga Pinheiro <olgapin@gmail.com>
2020-03-31 18:28:11 -0300

    Create Badge component, scss classes and add it to 'Icons and Bages' Story

---
+++ labsystem/src/Badge.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class Badge extends React.Component {
+  static propTypes = {
+    type: PropTypes.oneOf([
+      'floating-add',
+      'harvest',
+      'meta',
+      'ponto',
+      'star',
+    ]).isRequired,
+    color: PropTypes.oneOf([
+      'teal',
+      'mineral70',
+      'mineral20',
+    ]).isRequired,
+  }
+
+  render() {
+    const { type, color } = this.props;
+    return <span className={`lab-badge ${color} ${type}`} />;
+  };
+}
