16fb7af9082444de3debcabb54fa31e879ccb1da
Olga Pinheiro <olgapin@gmail.com>
2020-04-22 12:29:50 -0300

    Create Toggle react component (WIP)

---
+++ labsystem/src/Toggle.js
@@ -0,0 +1,25 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export default class Icon extends React.Component {
+  static propTypes = {
+    color: PropTypes.oneOf(["teal", "purple", "coral"]),
+    disabled: PropTypes.oneOf([true, false]),
+    toggleId: PropTypes.string.isRequired
+  };
+
+  static defaultProps = {
+    color: "teal",
+    disabled: "false"
+  };
+
+  render() {
+    const { color, toggleId, disabled } = this.props;
+    return (
+      <label className="toggle" htmlFor={toggleId}>
+        <input type="checkbox" id={toggleId} />
+        <span className="slider" />
+      </label>
+    );
+  }
+}
