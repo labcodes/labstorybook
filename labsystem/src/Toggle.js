ff00f2df6b9da508b7241c646fd323336bcf6bca
Olga Pinheiro <olgapin@gmail.com>
2020-04-23 13:35:45 -0300

    WIP

--- labsystem/src/Toggle.js
+++ labsystem/src/Toggle.js
@@ -1,23 +1,53 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { isUndefined } from "lodash";
 
-export default class Icon extends React.Component {
+export default class Toggle extends React.Component {
   static propTypes = {
     color: PropTypes.oneOf(["teal", "purple", "coral"]),
-    disabled: PropTypes.oneOf([true, false]),
-    toggleId: PropTypes.string.isRequired
+    disabled: PropTypes.bool,
+    defaultValue: PropTypes.bool,
+    value: PropTypes.bool,
+    name: PropTypes.string.isRequired,
   };
 
   static defaultProps = {
     color: "teal",
-    disabled: "false"
+    disabled: false,
+    defaultValue: undefined,
+    value: undefined,
   };
 
+  constructor(props) {
+    super(props);
+    const { defaultValue, value, name } = props;
+    if (!isUndefined(defaultValue) && !isUndefined(value)) {
+      console.warn(
+        `You are setting both value and defaultValue for input ${name} at the same time. We always initialize the toggle with defaultValue. Make sure this is the behaviour you want.`
+      );
+    }
+    this.state = {
+      localValue: !isUndefined(defaultValue) ? defaultValue : false,
+    };
+  }
+
+  // handleOnClick = () => {
+  //   this.setState()
+  // };
+
   render() {
-    const { color, toggleId, disabled } = this.props;
+    const { color, name, disabled, value } = this.props;
+    const { localValue } = this.state;
+    const isChecked = !isUndefined(value) ? value : localValue;
+
     return (
-      <label className="toggle" htmlFor={toggleId}>
-        <input type="checkbox" id={toggleId} />
+      <label className="toggle" htmlFor={name}>
+        <input
+          type="checkbox"
+          id={name}
+          checked={isChecked}
+          disabled={disabled}
+        />
         <span className={`slider ${color} ${disabled}`} />
       </label>
     );
