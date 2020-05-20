9adbaddaac4cb558673fcc67987dd231b94b1b3d
Débora Correia <debora@labcodes.com.br>
2020-05-20 18:14:18 -0300

    Add prefix, suffix and required as props in input.js

--- labsystem/src/Inputs.js
+++ labsystem/src/Inputs.js
@@ -1,15 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Icon from './Icon';
 
 export default class Inputs extends React.Component {
-  static propTypes = {
-    type: PropTypes.oneOf(['default', 'required', 'prefix', 'prefix-required'])
-  }
-
-  static defaultProps = {
-    type: 'default'
-  }
-
   render() {
     const {
       autocomplete,
@@ -20,8 +13,55 @@ export default class Inputs extends React.Component {
       disabled,
       value,
       icon,
-      iconColor
+      iconColor,
+      required,
+      helpMessage,
+      prefix,
+      suffix
     } = this.props;
+
+    let trailingIcon = null;
+    let message = null;
+    let requiredIcon = null;
+    let prefixArea = null;
+    let suffixArea = null;
+
+    if (icon) {
+      trailingIcon = (
+        <button className="input__icon">
+          <Icon type={icon} color={iconColor} />
+        </button>
+      );
+    }
+
+    if (required) {
+      requiredIcon = (
+        <span class="required"></span>
+      );
+      message = (
+        <div class="input__message input__message--required">{helpMessage}</div>
+      );
+    }
+
+    if (prefix) {
+      prefixArea = (
+        <span class="prefix">{prefix}</span>
+      );
+    }
+
+    if (suffix) {
+      suffixArea = (
+        <span class="suffix">{suffix}</span>
+      );
+    }
+
+    //Checked for value just to simulate the UI.
+    if (value == "Error") {
+      message = (
+        <div class="input__message input__message--error">Error message</div>
+      );
+    }
+
     return (
       <div className="input__wrapper">
         <input
@@ -31,6 +71,17 @@ export default class Inputs extends React.Component {
           placeholder=" "
           disabled={disabled}
           value={value} />
+
+        {trailingIcon}
+
+        {prefixArea}
+
+        {suffixArea}
+
+        {message}
+
+        {requiredIcon}
+
         <label for={id}>{label}</label>
       </div>
     )
