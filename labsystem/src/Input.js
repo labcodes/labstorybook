5b64a4048517ef2a34e708af870e709ec6153858
Rhanna Andrade <rhanna@labcodes.com.br>
2020-06-23 14:57:35 -0300

    Refactoring react on input component WIP

--- labsystem/src/Input.js
+++ labsystem/src/Input.js
@@ -1,16 +1,17 @@
 import React from "react";
 import Icon from "./Icon";
 import PropTypes from "prop-types";
+import { isUndefined } from "lodash";
 
 export default class Inputs extends React.Component {
   static propTypes = {
     theme: PropTypes.string,
-    className: PropTypes.string, 
+    className: PropTypes.string,
     id: PropTypes.string.isRequired,
     type: PropTypes.string,
     label: PropTypes.string.isRequired,
     disabled: PropTypes.bool,
-    value: PropTypes.string, // avaliar depois como resolver// 
+    value: PropTypes.string, // avaliar depois como resolver//
     icon: PropTypes.string,
     iconColor: PropTypes.string,
     required: PropTypes.bool,
@@ -19,15 +20,14 @@ export default class Inputs extends React.Component {
     suffix: PropTypes.string,
     isValid: PropTypes.bool,
     customErrorMsg: PropTypes.string,
-    customValidationMethod: PropTypes.func,
+    onChange: PropTypes.func,
   };
-
   static defaultProps = {
     theme: undefined,
     className: undefined,
     type: "text",
     disabled: false,
-    value: undefined, // avaliar depois como resolver// 
+    value: undefined, // avaliar depois como resolver//
     icon: undefined,
     iconColor: "mineral-70",
     required: false,
@@ -36,24 +36,16 @@ export default class Inputs extends React.Component {
     suffix: undefined,
     isValid: true,
     customErrorMsg: "Custom Error",
+    onChange: undefined,
   };
-
   constructor(props) {
     super(props);
     this.inputRef = React.createRef();
+    this.state = {
+      stateValue: '',
+    };
   }
-
-  componentDidMount() {
-    const { isValid, customErrorMsg } = this.props;
-    if (!isValid) {
-      const inputElement = this.inputRef.current;
-      console.log(
-        isValid, customErrorMsg
-      );
-      inputElement.setCustomValidity(customErrorMsg);
-    }
-  }
-
+  
   trailingIcon = () => {
     const { icon, iconColor } = this.props;
     return (icon ? (
@@ -62,38 +54,61 @@ export default class Inputs extends React.Component {
       </button>
     ) : "")
   };
-
   requiredIcon = () => {
     const { required } = this.props;
     return (required ? (
       <span className="lab-input__required-icon"></span>
       ) : "")
   };
-
   message = () => {
     const { helpMessage, isValid, customErrorMsg } = this.props;
-    console.log("MESSAGE", isValid, customErrorMsg);
-    if (!isValid) {
-      return <div className="lab-input__message lab-input__message--error"> { customErrorMsg } </div>
-    };        
-    if (helpMessage) { 
+    if (helpMessage) {
       return <div className="lab-input__message lab-input__message--required">{helpMessage}</div>
-    };    
-  };
+    };
+    if (!isValid) {
+      return <div className="lab-input__message lab-input__message--error"> {customErrorMsg} </div>
+    };
+    if (!isValid, helpMessage) {
+      return <div className="lab-input__message lab-input__message--error"> {customErrorMsg} </div>
+    };
 
+
+  };
   prefixArea = () => {
     const { prefix } = this.props;
     return (prefix ? (
       <span className="lab-input__prefix">{prefix}</span>
       ) : "")
   };
-
   suffixArea = () => {
     const { suffix } = this.props;
     return (suffix ? (
       <span className="lab-input__suffix">{suffix}</span>
       ) : "")
   };
+
+  handleOnChange = (e) => {
+    const { onChange } = this.props;
+    const inputElementValue = this.inputRef.current.value;
+
+    console.log("handleOnChange:", inputElementValue);
+    if (!isUndefined(onChange)) {
+      onChange(inputElementValue);
+    }
+    this.setState((state) => ({ stateValue: inputElementValue }));
+  };
+
+  // componentDidMount() {
+  //   const { isValid, customErrorMsg } = this.props;
+  //   if (!isValid) {
+  //     const inputElement = this.inputRef.current;
+  //     console.log(
+  //       isValid, customErrorMsg
+  //     );
+  //     inputElement.setCustomValidity(customErrorMsg);
+  //   }
+    
+  // }
 
 
   render() {
@@ -110,37 +125,36 @@ export default class Inputs extends React.Component {
       helpMessage,
       prefix,
       suffix,
+      onChange,
+      ...rest
     } = this.props;
 
+    const {stateValue} = this.state;
 
     return (
       <div className="lab-input">
         <input
-          className={`lab-input__field ${className}`}
+          className={`lab-input__field ${className || ""}`}
           id={id}
           type={type}
           placeholder=" "
           disabled={disabled}
           value={value}
           ref={this.inputRef}
+          onChange={this.handleOnChange}
+          {...rest}
         />
-
         {this.trailingIcon()}
-
         {this.prefixArea()}
-
         {this.suffixArea()}
-
         {this.message()}
-
         {this.requiredIcon()}
-
         <label
           className={`lab-input__label`}
           htmlFor={id}>{label}
         </label>
+
       </div>
     );
   }
-
 }
