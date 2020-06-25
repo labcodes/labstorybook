5e62f84fda8a85445e235bc3573fe50464985fdb
Breno Chamie <breno@labcodes.com.br>
2020-06-25 19:42:20 -0300

    buttons >> created: sizes, icon placement, dark and light exceptions. Updated: documentation, link button is now an <anchor> / storybook >> created: fragment div classes for using background color.

--- labsystem/src/Button.js
+++ labsystem/src/Button.js
@@ -1,9 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Icon from "./Icon";
 // import { isUndefined } from "loadash";
 
 export default class Button extends React.Component {
   static propTypes = {
+    exception: PropTypes.string,
+    icon: PropTypes.string,
     disabled: PropTypes.bool,
     type: PropTypes.string,
     size: PropTypes.string,
@@ -11,20 +14,30 @@ export default class Button extends React.Component {
   };
 
   static defaultProps = {
+    exception: undefined,
+    icon: undefined,
     disabled: false,
-    type: undefined,
-    size: undefined,
+    type: "default",
+    size: "normal",
     children: undefined,
   };
 
+  icon = () => {
+    const { icon } = this.props;
+    return ( icon ? (
+      <Icon type={icon} color="mineral20"/>
+    ) : "")
+  };
+
   render() {
-    const { children, disabled, type, size } = this.props;
+    const { children, disabled, type, size, icon, exception, } = this.props;
     return (
       <button
         type="button"
-        className={`btn btn--${type} btn--${size}`}
+        className={`btn btn--${type} btn--${size} btn--${exception}`}
         disabled={disabled}
-      >
+        >
+        {this.icon()}
         {children}
       </button>
     );
