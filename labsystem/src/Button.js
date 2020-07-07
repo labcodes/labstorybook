6948d8d46e1425b7a3a6f50ca8e9ec3f51dd5e0b
Débora Correia <debora@labcodes.com.br>
2020-07-07 13:53:08 -0300

    Adds icons to button using different colors according to the types and with petit size

--- labsystem/src/Button.js
+++ labsystem/src/Button.js
@@ -23,8 +23,66 @@ export default class Button extends React.Component {
   };
 
   icon = () => {
-    const { icon } = this.props;
-    return icon ? <Icon type={icon} color="mineral20" /> : "";
+    const { icon, type, disabled, exception } = this.props;
+    let returnIcon;
+    let color;
+
+    // Default buttons
+    if (type === "default") {
+      color = "white";
+    }
+    if (type === "default" && disabled) {
+      color = "mineral40";
+    }
+    // Default buttons with exceptions
+    if (type === "default" && exception === "dark") {
+      color = "white";
+    }
+    if (type === "default" && exception === "dark-warning") {
+      color = "yellow60";
+    }
+    if (type === "default" && exception === "light") {
+      color = "mineral90";
+    }
+    if (type === "default" && exception === "light-destructive") {
+      color = "red60";
+    }
+    if (type === "default" && exception === "light-confirmation") {
+      color = "green60";
+    }
+    // Ghost buttons
+    if (type === "ghost") {
+      color = "teal60";
+    }
+    if (type === "ghost" && disabled) {
+      color = "mineral30";
+    }
+    // Ghost buttons with exceptions
+    if (type === "ghost" && exception === "ghost-light") {
+      color = "white";
+    }
+    if (type === "ghost" && exception === "ghost-dark") {
+      color = "mineral90";
+    }
+    // Text buttons
+    if (type === "text") {
+      color = "teal60";
+    }
+    if (type === "text" && disabled) {
+      color = "mineral30";
+    }
+    // Destructive and warning buttons
+    if (type === "destructive") {
+      color = "white";
+    }
+    if (type === "warning") {
+      color = "black75";
+    }
+
+    if (icon) {
+      returnIcon = <Icon type={icon} color={color} size="petit" />;
+    }
+    return returnIcon;
   };
 
   render() {
