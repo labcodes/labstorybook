5a33d59745f68cbd1a9ac5e71b10a9a4151b4702
Olga Pinheiro <olgapin@gmail.com>
2020-07-10 08:45:14 -0300

    Update Badge component PropTypes to include all colors, wrapperColor and size

--- labsystem/src/Badge.js
+++ labsystem/src/Badge.js
@@ -5,7 +5,47 @@ export default class Badge extends React.Component {
   static propTypes = {
     type: PropTypes.oneOf(["floating-add", "harvest", "meta", "ponto", "star"])
       .isRequired,
-    color: PropTypes.oneOf(["teal", "mineral70", "mineral20"]).isRequired,
+    color: PropTypes.oneOf([
+      "white",
+      "black75",
+      "mineral10",
+      "mineral20",
+      "mineral30",
+      "mineral40",
+      "mineral60",
+      "mineral70",
+      "mineral80",
+      "mineral90",
+      "teal40",
+      "teal60",
+      "teal70",
+      "purple40",
+      "purple60",
+      "purple70",
+    ]).isRequired,
+    wrapperColor: PropTypes.oneOf([
+      "white",
+      "black75",
+      "mineral10",
+      "mineral20",
+      "mineral30",
+      "mineral40",
+      "mineral60",
+      "mineral70",
+      "mineral80",
+      "mineral90",
+      "teal40",
+      "teal60",
+      "teal70",
+      "purple40",
+      "purple60",
+      "purple70",
+    ]).isRequired,
+    size: PropTypes.oneOf(["small", "tiny"]),
+  };
+
+  static defaultProps = {
+    size: undefined,
   };
 
   render() {
