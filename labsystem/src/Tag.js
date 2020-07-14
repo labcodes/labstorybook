79ca61520549aebae7c4a4bdec84acc1f4f5c5ea
Débora Correia <debora@labcodes.com.br>
2020-07-14 12:31:03 -0300

    Adjust to create classes for teal, purple and mineral colors to use as soft and intense tags

--- labsystem/src/Tag.js
+++ labsystem/src/Tag.js
@@ -19,72 +19,105 @@ export default class Tag extends React.Component {
 
   static defaultProps = {
     children: undefined,
+    type: undefined,
     icon: undefined,
     thumbSrc: undefined,
     thumbAlt: undefined,
-    style: "fill",
+    style: "",
     colorStyle: "soft",
-    color: "teal",
+    color: "",
+    removable: undefined,
+    dropdown: undefined,
+    disabled: undefined,
   };
 
   icon = () => {
     const { icon } = this.props;
+    let returnIcon;
+
     if (icon) {
-      return (
-        <span class="lab-tag__left-icon">
-          <Icon type={ icon } color="black75" size="petit" />
+      returnIcon = (
+        <span className="lab-tag__left-icon">
+          <Icon type={icon} color="black75" size="petit" />
         </span>
       );
     }
+
+    return returnIcon;
   };
 
   thumb = () => {
     const { thumbSrc, thumbAlt } = this.props;
+    let returnThumb;
+
     if (thumbSrc && thumbAlt) {
-      return <img className="lab-tag__thumb" src={ thumbSrc } alt={ thumbAlt } />
+      returnThumb = (
+        <img className="lab-tag__thumb" src={thumbSrc} alt={thumbAlt} />
+      );
     }
+
+    return returnThumb;
   };
 
   dropdownIcon = () => {
     const { dropdown } = this.props;
+    let returnDropdownIcon;
+
     if (dropdown) {
-      return (
+      returnDropdownIcon = (
         <span className="lab-tag__right-icon lab-tag__right-icon--dropdown">
-          <Icon type="dropdown-closed" color="black75" size="petit"/>
+          <Icon type="dropdown-closed" color="black75" size="petit" />
         </span>
       );
     }
+
+    return returnDropdownIcon;
   };
 
   removeIcon = () => {
     const { removable } = this.props;
+    let returnRemoveIcon;
     if (removable) {
-      return (
+      returnRemoveIcon = (
         <span className="lab-tag__right-icon lab-tag__right-icon--remove">
-          <Icon type="remove" color="black75" size="petit"/>
+          <Icon type="remove" color="black75" size="petit" />
         </span>
       );
     }
+
+    return returnRemoveIcon;
   };
-  
+
   render() {
-    const { icon, children, thumbSrc, style, dropdown, removable, disabled, colorStyle, color } = this.props;
+    const {
+      icon,
+      children,
+      thumbSrc,
+      style,
+      dropdown,
+      removable,
+      disabled,
+      colorStyle,
+      color,
+    } = this.props;
     return (
-      <span 
+      <span
         className={`
-          lab-tag lab-tag--${style} lab-tag--${color}-${colorStyle}
-          ${icon ? "lab-tag--has-left-icon" : ''}
-          ${thumbSrc ? "lab-tag--has-thumb" : ''}
-          ${dropdown ? "lab-tag--has-right-icon" : ''}
-          ${removable ? "lab-tag--has-right-icon" : ''}
-          ${disabled ? "lab-tag--disabled" : ''}
+          lab-tag  lab-tag__skin-${colorStyle}
+          ${style ? `lab-tag--${style}` : ""}
+          ${color ? `lab-tag--${color}-${colorStyle}` : ""}
+          ${icon ? "lab-tag--has-left-icon" : ""}
+          ${thumbSrc ? "lab-tag--has-thumb" : ""}
+          ${dropdown ? "lab-tag--has-right-icon" : ""}
+          ${removable ? "lab-tag--has-right-icon" : ""}
+          ${disabled ? "lab-tag--disabled" : ""}
         `}
       >
-          { this.thumb() }
-          { this.icon() }
-          { children }
-          { this.dropdownIcon() }
-          { this.removeIcon() }
+        {this.thumb()}
+        {this.icon()}
+        {children}
+        {this.dropdownIcon()}
+        {this.removeIcon()}
       </span>
     );
   }
