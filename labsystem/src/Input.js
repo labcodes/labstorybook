dc1bb1d2c586e22e6f4af9f09579d7ffbd47aa0e
Luciano Ratamero <luciano@ratamero.com>
2020-07-20 16:40:41 -0300

    WIP on new animation and html structure for labels
    Co-authored-by: Débora Correia <debora@labcodes.com.br>

--- labsystem/src/Input.js
+++ labsystem/src/Input.js
@@ -1,7 +1,7 @@
 import React from "react";
-import Icon from "./Icon";
 import PropTypes from "prop-types";
 import { isUndefined } from "lodash";
+import Icon from "./Icon";
 
 export default class Inputs extends React.Component {
   static propTypes = {
@@ -54,68 +54,83 @@ export default class Inputs extends React.Component {
     }
     this.state = {
       localValue: defaultValue,
-      localIsValid: !isUndefined(isValid)? isValid : true,
+      localIsValid: !isUndefined(isValid) ? isValid : true,
     };
   }
-  
+
   componentDidMount() {
     const { customErrorMsg, defaultValue } = this.props;
     let { localIsValid } = this.state;
 
     if (defaultValue) {
       localIsValid = this.inputRef.current.validity.valid;
-      this.setState((state) => ({ localIsValid: localIsValid }));
+      this.setState((state) => ({ localIsValid }));
     }
     if (!localIsValid) {
       const inputElement = this.inputRef.current;
-      console.log(
-        localIsValid, customErrorMsg
-      );
+      console.log(localIsValid, customErrorMsg);
       inputElement.setCustomValidity(customErrorMsg);
     }
   }
-  
+
   trailingIcon = () => {
     const { icon, iconColor } = this.props;
-    return (icon ? (
+    return icon ? (
       <button className="lab-input__icon">
         <Icon type={icon} color={iconColor} />
       </button>
-    ) : "")
+    ) : (
+      ""
+    );
   };
+
   requiredIcon = () => {
     const { required } = this.props;
-    return (required ? (
+    return required ? (
       <span className="lab-input__required-icon">
-          <Icon type="star" color="white" />
+        <Icon type="star" color="white" />
       </span>
-      ) : "")
+    ) : (
+      ""
+    );
   };
+
   message = () => {
     const { helpMessage, customErrorMsg } = this.props;
     const { localIsValid } = this.state;
     if (helpMessage && localIsValid) {
-      return <div className="lab-input__message lab-input__message--required">{helpMessage}</div>
-    };
+      return (
+        <div className="lab-input__message lab-input__message--required">
+          {helpMessage}
+        </div>
+      );
+    }
     if (!localIsValid && helpMessage && !this.state.localValue) {
-      return <div className="lab-input__message lab-input__message--error"> {helpMessage} </div>
-    };  
+      return (
+        <div className="lab-input__message lab-input__message--error">
+          {" "}
+          {helpMessage}{" "}
+        </div>
+      );
+    }
     if (!localIsValid) {
-      return <div className="lab-input__message lab-input__message--error"> {customErrorMsg} </div>
-    };
-
+      return (
+        <div className="lab-input__message lab-input__message--error">
+          {" "}
+          {customErrorMsg}{" "}
+        </div>
+      );
+    }
   };
+
   prefixArea = () => {
     const { prefix } = this.props;
-    return (prefix ? (
-      <span className="lab-input__prefix">{prefix}</span>
-      ) : "")
+    return prefix ? <span className="lab-input__prefix">{prefix}</span> : "";
   };
+
   suffixArea = () => {
     const { suffix } = this.props;
-    return (suffix ? (
-      <span className="lab-input__suffix">{suffix}</span>
-      ) : "")
+    return suffix ? <span className="lab-input__suffix">{suffix}</span> : "";
   };
 
   handleOnChange = (e) => {
@@ -132,7 +147,6 @@ export default class Inputs extends React.Component {
       this.setState((state) => ({ localIsValid: inputElementIsValid }));
     }
   };
-
 
   render() {
     const {
@@ -156,29 +170,31 @@ export default class Inputs extends React.Component {
 
     return (
       <div className="lab-input">
-        <input
-          className={`lab-input__field ${className || ""}`}
-          id={id}
-          type={type}
-          placeholder=" "
-          disabled={disabled}
-          defaultValue={localValue}
-          value={value}
-          ref={this.inputRef}
-          onChange={this.handleOnChange}
-          {...rest}
-        />
-        {this.trailingIcon()}
-        {this.prefixArea()}
-        {this.suffixArea()}
+        <div className="lab-input__input-wrapper">
+          {this.prefixArea()}
+          <input
+            className={`lab-input__field ${className || ""}`}
+            id={id}
+            type={type}
+            placeholder=" "
+            disabled={disabled}
+            defaultValue={localValue}
+            value={value}
+            ref={this.inputRef}
+            onChange={this.handleOnChange}
+            {...rest}
+          />
+          {this.trailingIcon()}
+          {this.suffixArea()}
+          {this.requiredIcon()}
+          <div className="lab-input__label-wrapper">
+            {this.prefixArea()}
+            <label className="lab-input__label" htmlFor={id}>
+              {label}
+            </label>
+          </div>
+        </div>
         {this.message()}
-        {this.requiredIcon()}
-        <label
-          className={`lab-input__label`}
-          htmlFor={id}>{label}
-        </label>
-        {/* <p>Local value: {localValue}</p>
-        <p>Local isValid: { ""+localIsValid}</p> */}
       </div>
     );
   }
