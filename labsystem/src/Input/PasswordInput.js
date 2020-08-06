e1838992b606b9b158053c55ed021a1723d666cf
Olga Pinheiro <olgapin@gmail.com>
2020-08-06 18:18:23 -0300

    Create PasswordInput component using AbstractTextInput - WIP

---
+++ labsystem/src/Input/PasswordInput.js
@@ -0,0 +1,46 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import AbstractTextInput from "./AbstractTextInput";
+
+export default class PasswordInput extends React.Component {
+  static propTypes = {
+    id: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    theme: PropTypes.string,
+    className: PropTypes.string,
+    disabled: PropTypes.bool,
+    defaultValue: PropTypes.string,
+    value: PropTypes.string, // avaliar depois como resolver//
+    required: PropTypes.bool,
+    helpMessage: PropTypes.string,
+    prefix: PropTypes.string,
+    suffix: PropTypes.string,
+    isValid: PropTypes.bool,
+    customErrorMsg: PropTypes.string,
+    onChange: PropTypes.func,
+  };
+
+  static defaultProps = {
+    theme: undefined,
+    className: undefined,
+    disabled: false,
+    defaultValue: undefined,
+    value: undefined, // avaliar depois como resolver//
+    required: false,
+    helpMessage: undefined,
+    prefix: undefined,
+    suffix: undefined,
+    isValid: undefined,
+    customErrorMsg: "Custom Error",
+    onChange: undefined,
+  };
+
+  render() {
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    return (
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      <AbstractTextInput type="password" icon="eye-closed" {...this.props} />
+    );
+  }
+}
