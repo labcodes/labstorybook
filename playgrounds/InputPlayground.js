f4c52aebbb7e7d6ba5cc9b6b002c280cdaa5e920
Olga Pinheiro <olgapin@gmail.com>
2020-08-11 13:42:16 -0300

    Fix InputPlayground accessibility issues

--- playgrounds/InputPlayground.js
+++ playgrounds/InputPlayground.js
@@ -78,126 +78,148 @@ export default class InputPlayground extends React.Component {
         <div className="column lab-playground__configs">
           <h4>Configurations</h4>
           <span className="lab-playground__item">
-            <label htmlFor="label">Label</label>
-            <br />
-            <input
-              id="label"
-              type="text"
-              label="Label"
-              value={label}
-              onChange={this.handlePropChangeText}
-            />
+            <label htmlFor="label">
+              Label
+              <br />
+              <input
+                id="label"
+                type="text"
+                label="Label"
+                value={label}
+                onChange={this.handlePropChangeText}
+              />
+            </label>
           </span>
           <span className="lab-playground__item">
-            <label htmlFor="className">className</label>
-            <br />
-            <input
-              id="className"
-              type="text"
-              label="className"
-              value={className}
-              onChange={this.handlePropChangeText}
-            />
+            <label htmlFor="className">
+              className
+              <br />
+              <input
+                id="className"
+                type="text"
+                label="className"
+                value={className}
+                onChange={this.handlePropChangeText}
+              />
+            </label>
           </span>
           <span className="lab-playground__item">
-            <label htmlFor="icon">icon</label>
-            <br />
-            <input
-              id="icon"
-              type="text"
-              label="icon"
-              value={icon}
-              onChange={this.handlePropChangeText}
-            />
+            <label htmlFor="icon">
+              icon
+              <br />
+              <input
+                id="icon"
+                type="text"
+                label="icon"
+                value={icon}
+                onChange={this.handlePropChangeText}
+              />
+            </label>
           </span>
           <span className="lab-playground__item">
-            <label htmlFor="iconColor">iconColor</label>
-            <br />
-            <input
-              id="iconColor"
-              type="text"
-              label="iconColor"
-              value={iconColor}
-              onChange={this.handlePropChangeText}
-            />
+            <label htmlFor="iconColor">
+              iconColor
+              <br />
+              <input
+                id="iconColor"
+                type="text"
+                label="iconColor"
+                value={iconColor}
+                onChange={this.handlePropChangeText}
+              />
+            </label>
           </span>
           <span className="lab-playground__item">
-            <label htmlFor="helpMessage">helpMessage</label>
-            <br />
-            <input
-              id="helpMessage"
-              type="text"
-              label="helpMessage"
-              value={helpMessage}
-              onChange={this.handlePropChangeText}
-            />
+            <label htmlFor="helpMessage">
+              helpMessage
+              <br />
+              <input
+                id="helpMessage"
+                type="text"
+                label="helpMessage"
+                value={helpMessage}
+                onChange={this.handlePropChangeText}
+              />
+            </label>
           </span>
           <span className="lab-playground__item">
-            <label htmlFor="customErrorMsg">customErrorMsg</label>
-            <br />
-            <input
-              id="customErrorMsg"
-              type="text"
-              label="customErrorMsg"
-              value={customErrorMsg}
-              onChange={this.handlePropChangeText}
-            />
+            <label htmlFor="customErrorMsg">
+              customErrorMsg
+              <br />
+              <input
+                id="customErrorMsg"
+                type="text"
+                label="customErrorMsg"
+                value={customErrorMsg}
+                onChange={this.handlePropChangeText}
+              />
+            </label>
           </span>
           <span className="lab-playground__item">
-            <label htmlFor="prefix">Prefix</label>
-            <br />
-            <input
-              id="prefix"
-              type="text"
-              label="prefix"
-              value={prefix}
-              onChange={this.handlePropChangeText}
-            />
+            <label htmlFor="prefix">
+              Prefix
+              <br />
+              <input
+                id="prefix"
+                type="text"
+                label="prefix"
+                value={prefix}
+                onChange={this.handlePropChangeText}
+              />
+            </label>
           </span>
           <span className="lab-playground__item">
-            <label htmlFor="suffix">Suffix</label>
-            <br />
-            <input
-              id="suffix"
-              type="text"
-              label="suffix"
-              value={suffix}
-              onChange={this.handlePropChangeText}
-            />
+            <label htmlFor="suffix">
+              Suffix
+              <br />
+              <input
+                id="suffix"
+                type="text"
+                label="suffix"
+                value={suffix}
+                onChange={this.handlePropChangeText}
+              />
+            </label>
           </span>
           <br />
           <span className="lab-playground__item">
-            <label htmlFor="required">Required</label>
-            <br />
-            <input
-              id="required"
-              type="checkbox"
-              label="Required"
-              checked={required}
-              onChange={this.handlePropChangeBool}
-            />
+            <label htmlFor="required">
+              Required
+              <br />
+              <input
+                id="required"
+                type="checkbox"
+                label="Required"
+                checked={required}
+                onChange={this.handlePropChangeBool}
+              />
+            </label>
           </span>
           <span className="lab-playground__item">
-            <label htmlFor="disabled">Disabled</label>
-            <br />
-            <input
-              id="disabled"
-              type="checkbox"
-              label="Disabled"
-              checked={disabled}
-              onChange={this.handlePropChangeBool}
-            />
+            <label htmlFor="disabled">
+              Disabled
+              <br />
+              <input
+                id="disabled"
+                type="checkbox"
+                label="Disabled"
+                checked={disabled}
+                onChange={this.handlePropChangeBool}
+              />
+            </label>
           </span>
           <span className="lab-playground__item">
-            <label htmlFor="isValid">isValid</label>
-            <br />
-            <input
-              id="isValid"
-              type="checkbox"
-              label="IsValid"
-              checked={isValid}
-              onChange={this.handlePropChangeBool}
-            />
+            <label htmlFor="isValid">
+              isValid
+              <br />
+              <input
+                id="isValid"
+                type="checkbox"
+                label="IsValid"
+                checked={isValid}
+                onChange={this.handlePropChangeBool}
+              />
+            </label>
           </span>
         </div>
       </div>
