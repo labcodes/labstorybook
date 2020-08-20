2fc1cabef9d0ba87c2b7d409086119fb1baf15b3
Olga Pinheiro <olgapin@gmail.com>
2020-08-20 18:02:58 -0300

    Update Text Input tests

--- labsystem/src/Input/Input.test.js
+++ labsystem/src/Input/Input.test.js
@@ -22,8 +22,84 @@ describe("AbstractTextInput", () => {
       />
     );
     expect(renderedComponent.find("input[disabled]")).toHaveLength(1);
-    expect(renderedComponent.find("input"));
-    console.log(renderedComponent.find("input[disabled]").value);
-    // expect(renderedComponent).toMatchSnapshot();
+  });
+
+  it("sets state with defaultValue if it is passed by props", async () => {
+    const component = mount(
+      <AbstractTextInput
+        id="testInput"
+        label="Test Input"
+        defaultValue="default value"
+        value="test value"
+      />
+    );
+    expect(component.state().localValue).toBe("default value");
+  });
+
+  it("sets state with isValid if it is passed by props", async () => {
+    const component = mount(
+      <AbstractTextInput
+        id="testInput"
+        label="Test Input"
+        defaultValue="default value"
+        isValid={false}
+      />
+    );
+    expect(component.state().localIsValid).toBe(false);
+  });
+
+  it("sets localIsValid to false if defaultValue is invalid", async () => {
+    const component = mount(
+      <AbstractTextInput
+        id="testInput"
+        label="Test Input"
+        type="email"
+        defaultValue="default value"
+      />
+    );
+    expect(component.state().localIsValid).toBe(false);
+  });
+
+  it("sets localIsValid to false if value is invalid", async () => {
+    const component = mount(
+      <AbstractTextInput
+        id="testInput"
+        label="Test Input"
+        type="email"
+        value="test value"
+      />
+    );
+    expect(component.state().localIsValid).toBe(false);
+  });
+
+  it("sets localIsValid to true if value is valid and change it if value validity changes", async () => {
+    const component = mount(
+      <AbstractTextInput
+        id="testInput"
+        label="Test Input"
+        type="email"
+        value="testvalue@g.com"
+      />
+    );
+    expect(component.state().localIsValid).toBe(true);
+
+    component.setProps({ value: "value changed" });
+
+    expect(component.state().localIsValid).toBe(false);
+  });
+
+  it("changes localValue state when input changes", async () => {
+    const component = mount(
+      <AbstractTextInput id="testInput" label="Test Input" name="testName" />
+    );
+
+    expect(component.state().localValue).toBe(undefined);
+    component
+      .find("input")
+      .at(0)
+      .simulate("change", {
+        target: { value: "My new value", validity: { valid: true } },
+      });
+    expect(component.state().localValue).toBe("My new value");
   });
 });
