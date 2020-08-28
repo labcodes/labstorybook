ab74072a091efd69ce19b45f9d2a7891c9dac940
Olga Pinheiro <olgapin@gmail.com>
2020-08-28 18:26:13 -0300

    Create Tooltip stories

--- labsystem/src/Tooltip.js
+++ labsystem/src/Tooltip.js
@@ -2,8 +2,12 @@ import React from "react";
 
 export default class Tooltip extends React.Component {
   render() {
-    const { text, color } = this.props;
+    const { id, text, color } = this.props;
 
-    return <div className={`lab-tooltip lab-tooltip--${color}`}>{text}</div>;
+    return (
+      <div className={`lab-tooltip lab-tooltip--${color}`} id={id}>
+        {text}
+      </div>
+    );
   }
 }
