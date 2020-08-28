322766ba210d22d8ad087130586f2fd827041aa4
Olga Pinheiro <olgapin@gmail.com>
2020-08-28 19:10:22 -0300

    Add PropTypes validation to Tooltip component

--- labsystem/src/Tooltip.js
+++ labsystem/src/Tooltip.js
@@ -1,6 +1,17 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 export default class Tooltip extends React.Component {
+  static propTypes = {
+    id: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired,
+    color: PropTypes.string,
+  };
+
+  static defaultProps = {
+    color: undefined,
+  };
+
   render() {
     const { id, text, color } = this.props;
 
