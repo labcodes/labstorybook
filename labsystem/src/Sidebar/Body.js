bf9ccfd5e935cc48d94f9ae86fbe3b81c6bede21
Rhanna Andrade <rhanna@labcodes.com.br>
2020-09-03 16:35:57 -0300

    Add CSS mixins to reduce declarations for Vivid Skin sidebars

--- labsystem/src/Sidebar/Body.js
+++ labsystem/src/Sidebar/Body.js
@@ -4,15 +4,27 @@ import PropTypes from "prop-types";
 export default class Body extends React.Component {
   static propTypes = {
     children: PropTypes.element,
+    className: PropTypes.string,
+    withScroll: PropTypes.bool,
     
   };
 
   static defaultProps = {
     children: undefined,
+    className: undefined,
+    withScroll: false,
   };
 
   render() {
-    const { children } = this.props;
-    return <nav className="lab-narrow-sidebar__body"> {children}</nav>;
+    const { children, className, withScroll } = this.props;
+    return (
+    <nav 
+    className={
+      `lab-narrow-sidebar__body`+
+      `${withScroll ? ` lab-narrow-sidebar__body--with-scroll` : ` ""`}`
+    }
+    > {children}
+    </nav>
+    );  
   }
 }
--- labsystem/src/Sidebar/Header.js
+++ labsystem/src/Sidebar/Header.js
@@ -3,22 +3,15 @@ import PropTypes from "prop-types";
 
 export default class Header extends React.Component {
   static propTypes = {
-    logoSrc: PropTypes.string,
-    altText: PropTypes.string,
+    children: PropTypes.element,
   };
 
   static defaultProps = {
-    logoSrc: undefined,
-    altText: undefined,
+    children: undefined,
   };
 
   render() {
-    const { logoSrc, altText } = this.props;
-    return (
-      <div className="lab-narrow-sidebar__header">
-        <div className="lab-narrow-sidebar__logo" />
-        {/* <img className="lab-narrow-sidebar__logo" src={logoSrc} alt={altText} /> */}
-      </div>
-    );
+    const { children } = this.props;
+    return <div className="lab-narrow-sidebar__header"> {children}</div>;
   }
 }
