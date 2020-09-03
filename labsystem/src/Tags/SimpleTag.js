1f48596b4a7a54996d2fa6730b4715d24366e406
cavalcantelucas <lucas@labcodes.com.br>
2020-09-03 20:22:02 -0300

    Refactor 'SimpleTag'

--- labsystem/src/Tags/SimpleTag.js
+++ labsystem/src/Tags/SimpleTag.js
@@ -1,9 +1,55 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { isEmpty } from "lodash";
 import AbstractTag from "./AbstractTag";
 
 export default class SimpleTag extends React.Component {
+  static propTypes = {
+    text: PropTypes.string.isRequired,
+    thumbSrc: PropTypes.string,
+    icon: PropTypes.string,
+    outline: PropTypes.bool,
+    skin: PropTypes.string,
+    color: PropTypes.string,
+  };
+
+  static defaultProps = {
+    thumbSrc: "",
+    icon: "",
+    outline: false,
+    skin: "pale",
+    color: "",
+  };
+
+  constructor(props) {
+    super(props);
+    this.checkThumbAndIcon();
+  }
+
+  componentDidUpdate() {
+    this.checkThumbAndIcon();
+  }
+
+  checkThumbAndIcon() {
+    const errorMessage =
+      "<SimpleTag> can't be initialized with both `thumb` and `icon` props.";
+    const { thumbSrc, icon } = this.props;
+    if (!isEmpty(thumbSrc) && !isEmpty(icon)) {
+      throw Error(errorMessage);
+    }
+  }
+
   render() {
-    // eslint-disable-next-line react/jsx-props-no-spreading
-    return <AbstractTag {...this.props} />;
+    const { text, thumbSrc, icon, outline, skin, color } = this.props;
+    return (
+      <AbstractTag
+        text={text}
+        thumbSrc={thumbSrc}
+        icon={icon}
+        outline={outline}
+        skin={skin}
+        color={color}
+      />
+    );
   }
 }
