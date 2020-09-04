05b3f9c2bbef3f123d53c6d0abfe14f48721aa76
cavalcantelucas <lucas@labcodes.com.br>
2020-09-04 15:29:35 -0300

    Introduce 'ToggableTag'

---
+++ labsystem/src/Tags/ToggableTag.js
@@ -0,0 +1,9 @@
+import React from "react";
+import AbstractTag from "./AbstractTag";
+
+export default class ToggableTag extends React.Component {
+  render() {
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    return <AbstractTag {...this.props} />;
+  }
+}
--- labsystem/src/Tags/index.js
+++ labsystem/src/Tags/index.js
@@ -1,2 +1,3 @@
 export { default as Tag } from "./Tag";
 export { default as SimpleTag } from "./SimpleTag";
+export { default as ToggableTag } from "./ToggableTag";
