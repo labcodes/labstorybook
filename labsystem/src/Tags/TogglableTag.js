a1bc0f79879eceb7726dea781f509d0218a06e4f
cavalcantelucas <lucas@labcodes.com.br>
2020-09-09 14:15:26 -0300

    Improve behavior for when clicking on a 'TogglableTag'

--- labsystem/src/Tags/TogglableTag.js
+++ labsystem/src/Tags/TogglableTag.js
@@ -5,22 +5,35 @@ import AbstractTag from "./AbstractTag";
 export default class TogglableTag extends React.Component {
   static propTypes = {
     text: PropTypes.string.isRequired,
+    color: PropTypes.string,
   };
 
+  static defaultProps = {
+    color: "",
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOn: true,
+    };
+  }
+
   onClick = () => {
-    const { text } = this.props;
-    // eslint-disable-next-line no-alert
-    alert(text);
+    const { isOn } = this.state;
+    this.setState({ isOn: !isOn });
   };
 
   render() {
-    const { text } = this.props;
+    const { text, color } = this.props;
+    const { isOn } = this.state;
     return (
       <AbstractTag
         togglable
         text={text}
+        color={color}
         onClick={this.onClick}
-        {...this.props}
+        skin={isOn ? "pale" : "vivid"}
       />
     );
   }
