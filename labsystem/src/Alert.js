2b20d2d3709fd452766e1cb636b9c57d7f4e4977
Daniela Falcone <danielafalcone@gmail.com>
2020-09-10 19:03:07 -0300

    added alert types color schemes

--- labsystem/src/Alert.js
+++ labsystem/src/Alert.js
@@ -4,21 +4,17 @@ import PropTypes from "prop-types";
 export default class Alert extends React.Component {
   static propTypes = {
     text: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired,
-    // type: PropTypes.oneOf([info, warning, error]).isRequired,
+    type: PropTypes.oneOf(["info", "warn", "error"]),
+    // icon: PropTypes.string.isRequired,
     // buttonText: PropTypes.string.isRequired,
   };
 
-  // static defaultProps = {
-  //   buttonText: "check",
-  // };
+  static defaultProps = {
+    type: "info",
+  };
 
   render() {
-    const { text, icon } = this.props;
-    return (
-      <span className="lab-alert">
-        {text}
-      </span>
-    );
+    const { text, type } = this.props;
+    return <span className={`lab-alert__${type}`}>{text}</span>;
   }
 }
