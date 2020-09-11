92fbe7779ce5812fbf83ae425a4f570078b4c662
cavalcantelucas <lucas@labcodes.com.br>
2020-09-11 12:22:41 -0300

    Introduce tests for 'AbstractTag'

---
+++ labsystem/src/Tags/AbstractTag.test.js
@@ -0,0 +1,34 @@
+import React from "react";
+import renderer from "react-test-renderer";
+
+import AbstractTag from "./AbstractTag";
+
+describe("AbstractTag", () => {
+  it("renders with base props for 'simple' type", async () => {
+    const renderedComponent = renderer
+      .create(<AbstractTag text="Test SimpleTag" />)
+      .toJSON();
+    expect(renderedComponent).toMatchSnapshot();
+  });
+
+  it("renders with base props for 'removable' type", async () => {
+    const renderedComponent = renderer
+      .create(<AbstractTag removable text="Test RemovableTag" />)
+      .toJSON();
+    expect(renderedComponent).toMatchSnapshot();
+  });
+
+  it("renders with base props for 'togglable' type", async () => {
+    const renderedComponent = renderer
+      .create(<AbstractTag togglable text="Test TogglableTag" />)
+      .toJSON();
+    expect(renderedComponent).toMatchSnapshot();
+  });
+
+  it("renders with base props for 'dropdown' type", async () => {
+    const renderedComponent = renderer
+      .create(<AbstractTag dropdown text="Test DropdownTag" />)
+      .toJSON();
+    expect(renderedComponent).toMatchSnapshot();
+  });
+});
