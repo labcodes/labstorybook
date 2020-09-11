33a15a7163348176d3d2609bf2f30ecccbde345e
cavalcantelucas <lucas@labcodes.com.br>
2020-09-11 16:44:40 -0300

    Prune 'Tags'

--- labsystem/src/Tags/Tag.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import AbstractTag from "./AbstractTag";
-
-export default class Tag extends React.Component {
-  render() {
-    // eslint-disable-next-line react/jsx-props-no-spreading
-    return <AbstractTag {...this.props} />;
-  }
-}
--- labsystem/src/Tags/index.js
+++ labsystem/src/Tags/index.js
@@ -1,4 +1,3 @@
-export { default as Tag } from "./Tag";
 export { default as SimpleTag } from "./SimpleTag";
 export { default as TogglableTag } from "./TogglableTag";
 export { default as RemovableTag } from "./RemovableTag";
