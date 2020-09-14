3c4404aa02d58bd392f1179613bd29f4084c7728
Rhanna Andrade <rhanna@labcodes.com.br>
2020-09-14 11:12:12 -0300

    change CSS declaration to style With-scroll

--- labsystem/src/Sidebar/Narrow-Sidebar.js
+++ labsystem/src/Sidebar/Narrow-Sidebar.js
@@ -6,6 +6,7 @@ export default class NarrowSidebar extends React.Component {
     children: PropTypes.element,
     vivid: PropTypes.bool,
     color: PropTypes.string,
+    withScroll: PropTypes.bool,
   };
 
   
@@ -13,6 +14,7 @@ export default class NarrowSidebar extends React.Component {
     children: undefined,
     vivid: false,
     color: "",
+    withScroll: false,
   };
 
 
@@ -22,13 +24,15 @@ export default class NarrowSidebar extends React.Component {
       children,
       vivid,
       color,
+      withScroll,
      } = this.props;
     return (
       <div 
         
         className={
           `lab-narrow-sidebar`+
-          `${vivid ? ` lab-narrow-sidebar--vivid lab-narrow-sidebar--vivid--${color}` : ` ""`}`
+          `${vivid ? ` lab-narrow-sidebar--vivid lab-narrow-sidebar--vivid--${color}` : ` ""`}`+
+          `${withScroll ? ` lab-narrow-sidebar--with-scroll` : ` ""`}`
         }
       >
         {children}
