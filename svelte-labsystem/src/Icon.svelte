215252096cd475a07aa2f79af4f711607224e0fb
Luciano Ratamero <luciano@ratamero.com>
2020-10-02 16:47:44 -0300

    Add Icon component

---
+++ svelte-labsystem/src/Icon.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let type = undefined;
+  export let size = undefined;
+  export let color = undefined;
+
+  let className = '';
+  export { className as class };
+</script>
+
+<span
+  class={`lab-icon lab-icon--${type} lab-icon--${color}` + `${size ? ` lab-icon--${size}` : ''}` + ` ${className}`} />
