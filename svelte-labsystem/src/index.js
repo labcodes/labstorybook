abfa05818c610410315f915581aa2bcf826cb7a2
Luciano Ratamero <luciano@ratamero.com>
2020-10-05 13:44:29 -0300

    Add missing exports from Icon/Badges to index.js

--- svelte-labsystem/src/index.js
+++ svelte-labsystem/src/index.js
@@ -1,2 +1,4 @@
 // here, we import each component and export them accordingly
 export { default as Toggle } from "./Toggle.svelte";
+export { default as Icon } from "./Icon.svelte";
+export { default as Badge } from "./Badge.svelte";
