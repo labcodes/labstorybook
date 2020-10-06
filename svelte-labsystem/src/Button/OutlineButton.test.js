9ddd6fae812f94e02f25ed8b4ce8cdc718167ef1
Luciano Ratamero <luciano@ratamero.com>
2020-10-06 19:24:07 -0300

    add tests for OutlineButton

--- svelte-labsystem/src/Button/OutlineButton.test.js
+++ svelte-labsystem/src/Button/OutlineButton.test.js
@@ -1,14 +1,14 @@
 import { render } from "@testing-library/svelte";
-import Button from "./Button.svelte";
+import OutlineButton from "./OutlineButton.svelte";
 
-describe("Button", () => {
+describe("OutlineButton", () => {
   it("renders with base props", async () => {
-    const { container } = render(Button, { text: "test" });
+    const { container } = render(OutlineButton, { text: "test" });
     expect(container).toMatchSnapshot();
   });
 
   it("renders as expected when passing disabled as true", async () => {
-    const { container, getByText } = render(Button, {
+    const { container, getByText } = render(OutlineButton, {
       text: "test",
       disabled: true,
     });
@@ -17,23 +17,35 @@ describe("Button", () => {
     expect(getByText("test").disabled).toBeTruthy();
   });
 
-  it("renders as expected when passing a warning skin", async () => {
-    const { container } = render(Button, { text: "test", skin: "warning" });
+  it("renders as expected when passing a light skin", async () => {
+    const { container } = render(OutlineButton, {
+      text: "test",
+      skin: "light",
+    });
     expect(container).toMatchSnapshot();
   });
 
   it("renders as expected when passing a small size", async () => {
-    const { container } = render(Button, { text: "test", size: "small" });
+    const { container } = render(OutlineButton, {
+      text: "test",
+      size: "small",
+    });
     expect(container).toMatchSnapshot();
   });
 
   it("renders as expected when passing a wallet icon", async () => {
-    const { container } = render(Button, { text: "test", icon: "wallet" });
+    const { container } = render(OutlineButton, {
+      text: "test",
+      icon: "wallet",
+    });
     expect(container).toMatchSnapshot();
   });
 
   it("renders as expected when passing fullWidth prop", async () => {
-    const { container } = render(Button, { text: "test", fullWidth: true });
+    const { container } = render(OutlineButton, {
+      text: "test",
+      fullWidth: true,
+    });
     expect(container).toMatchSnapshot();
   });
 });
