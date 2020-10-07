624daaa69384c498ac374070eca3fdc4716f5f7e
Luciano Ratamero <luciano@ratamero.com>
2020-10-06 22:18:44 -0300

    Improve docs styles

--- svelte-labsystem/src/DevApp.svelte
+++ svelte-labsystem/src/DevApp.svelte
@@ -25,233 +25,249 @@
     rel="stylesheet" />
 </svelte:head>
 
-<main>
-  <h1>Component samples</h1>
+<main class="container">
+  <div class="column is-half is-offset-one-quarter">
+    <h1>Component samples</h1>
 
-  <h2>Toggle:</h2>
-  <Toggle
-    name="sample-toggle"
-    bind:value={toggleValue}
-    on:change={(e) => console.log('Toggle changed:', e)} />
-  <Toggle
-    name="sample-toggle"
-    disabled
-    bind:value={toggleValue}
-    on:change={(e) => console.log('Toggle changed:', e)} />
-
-  <h2>Badge:</h2>
-  <Badge type="harvest" wrapperColor="teal-40" color="white" />
-  <Badge type="ponto" wrapperColor="teal-40" color="white" />
-  <Badge type="meta" wrapperColor="teal-40" color="white" />
-  <br />
-  <Badge type="star" wrapperColor="teal-40" color="white" />
-  <Badge type="floating-add" wrapperColor="teal-40" color="white" />
-
-  <h2>Icon:</h2>
-  <Icon type="arrow-left" color="teal-40" />
-  <Icon type="dropdown-closed" color="teal-40" />
-  <Icon type="remove" color="teal-40" />
-  <Icon type="eye-opened" color="teal-40" />
-  <Icon type="logout" color="teal-40" />
-  <Icon type="edit" color="teal-40" />
-  <Icon type="arrow-fill-right" color="teal-40" />
-  <br />
-  <Icon type="arrow-right" color="teal-40" />
-  <Icon type="dropdown-open" color="teal-40" />
-  <Icon type="plus" color="teal-40" />
-  <Icon type="eye-closed" color="teal-40" />
-  <Icon type="track" color="teal-40" />
-  <Icon type="key" color="teal-40" />
-  <Icon type="arrow-fill-left" color="teal-40" />
-  <br />
-  <Icon type="arrow-top" color="teal-40" />
-  <Icon type="collapse-closed" color="teal-40" />
-  <Icon type="minus" color="teal-40" />
-  <Icon type="lupe" color="teal-40" />
-  <Icon type="calendar" color="teal-40" />
-  <Icon type="upload" color="teal-40" />
-  <Icon type="chevron-right" color="teal-40" />
-  <br />
-  <Icon type="arrow-down" color="teal-40" />
-  <Icon type="collapse-open" color="teal-40" />
-  <Icon type="check" color="teal-40" />
-  <Icon type="trash" color="teal-40" />
-  <Icon type="coin" color="teal-40" />
-  <Icon type="reload" color="teal-40" />
-  <Icon type="chevron-left" color="teal-40" />
-  <br />
-  <Icon type="sort" color="teal-40" />
-  <Icon type="menu-expand" color="teal-40" />
-  <Icon type="menu-collapse" color="teal-40" />
-  <Icon type="menu-default" color="teal-40" />
-  <Icon type="external" color="teal-40" />
-  <Icon type="wallet" color="teal-40" />
-  <Icon type="workspace" color="teal-40" />
-
-  <h2>Tooltip:</h2>
-  <div class="columns docs-tooltip">
-    <div class="column">
-      <Tooltip id="tooltip-top-start" text="top-start" placement="top-start">
-        <button type="button"> top-start </button>
-      </Tooltip>
-    </div>
-    <div class="column">
-      <Tooltip id="tooltip-top" text="top" placement="top">
-        <button type="button"> top </button>
-      </Tooltip>
-    </div>
-    <div class="column">
-      <Tooltip id="tooltip-top-end" text="top-end" placement="top-end">
-        <button type="button"> top-end </button>
-      </Tooltip>
-    </div>
-  </div>
-  <div class="columns space-between docs-tooltip">
-    <div class="column left">
-      <Tooltip id="tooltip-left-start" text="left-start" placement="left-start">
-        <button type="button"> left-start </button>
-      </Tooltip>
-    </div>
-    <div class="column right">
-      <Tooltip
-        id="tooltip-right-start"
-        text="right-start"
-        placement="right-start">
-        <button type="button"> right-start </button>
-      </Tooltip>
-    </div>
-  </div>
-  <div class="columns space-between docs-tooltip">
-    <div class="column left">
-      <Tooltip id="tooltip-left" text="left" placement="left">
-        <button type="button"> left </button>
-      </Tooltip>
-    </div>
-    <div class="column right">
-      <Tooltip id="tooltip-right" text="right" placement="right">
-        <button type="button"> right </button>
-      </Tooltip>
-    </div>
-  </div>
-  <div class="columns space-between docs-tooltip">
-    <div class="column left">
-      <Tooltip id="tooltip-left-end" text="left-end" placement="left-end">
-        <button type="button"> left-end </button>
-      </Tooltip>
-    </div>
-    <div class="column right">
-      <Tooltip id="tooltip-right-end" text="right-end" placement="right-end">
-        <button type="button"> right-end </button>
-      </Tooltip>
-    </div>
-  </div>
-  <div class="columns docs-tooltip">
-    <div class="column">
-      <Tooltip
-        id="tooltip-bottom-start"
-        text="bottom-start"
-        placement="bottom-start">
-        <button type="button"> bottom-start </button>
-      </Tooltip>
-    </div>
-    <div class="column">
-      <Tooltip id="tooltip-bottom" text="bottom" placement="bottom">
-        <button type="button"> bottom </button>
-      </Tooltip>
-    </div>
-    <div class="column">
-      <Tooltip id="tooltip-bottom-end" text="bottom-end" placement="bottom-end">
-        <button type="button"> bottom-end </button>
-      </Tooltip>
-    </div>
-  </div>
-
-  <h2>Checkbox:</h2>
-  <fieldset>
-    <Checkbox id="checkbox-1" label="Idle" name="checkbox" checked />
-    <br />
-    <Checkbox
-      id="checkbox-2"
-      label="Indeterminate"
-      name="checkbox"
-      indeterminate
-      checked />
-    <br />
-    <Checkbox
-      id="checkbox-3"
-      label="Disabled Checked"
-      name="checkbox"
-      checked
-      disabled />
-    <br />
-    <Checkbox id="checkbox-4" label="Disabled Idle" name="checkbox" disabled />
-  </fieldset>
-
-  <h2>Radio:</h2>
-  <fieldset>
-    <Radio
-      id="radio-1"
-      label="Selected by default"
-      name="radio-group"
-      value="test-value"
-      bind:group={radioButtonsTestValue} />
-    <br />
-    <Radio
-      id="radio-2"
-      label="Idle"
-      name="radio-group"
-      value="test-value-2"
-      bind:group={radioButtonsTestValue} />
-    <br />
-    <Radio
-      id="radio-3"
-      label="Disabled - Checked if first is selected"
-      name="radio-group-2"
+    <h2>Toggle:</h2>
+    <Toggle
+      name="sample-toggle"
+      bind:value={toggleValue}
+      on:change={(e) => console.log('Toggle changed:', e)} />
+    <Toggle
+      name="sample-toggle"
       disabled
-      bind:group={radioButtonsTestValue}
-      value="test-value" />
-  </fieldset>
+      bind:value={toggleValue}
+      on:change={(e) => console.log('Toggle changed:', e)} />
 
-  <h2>Button:</h2>
-  <Button text="Default" />
-  <Button text="Default" icon="arrow-down" />
+    <h2>Badge:</h2>
+    <Badge type="harvest" wrapperColor="teal-40" color="white" />
+    <Badge type="ponto" wrapperColor="teal-40" color="white" />
+    <Badge type="meta" wrapperColor="teal-40" color="white" />
+    <br />
+    <Badge type="star" wrapperColor="teal-40" color="white" />
+    <Badge type="floating-add" wrapperColor="teal-40" color="white" />
 
-  <OutlineButton text="Default" />
-  <OutlineButton text="Default" icon="arrow-down" />
+    <h2>Icon:</h2>
+    <Icon type="arrow-left" color="teal-40" />
+    <Icon type="dropdown-closed" color="teal-40" />
+    <Icon type="remove" color="teal-40" />
+    <Icon type="eye-opened" color="teal-40" />
+    <Icon type="logout" color="teal-40" />
+    <Icon type="edit" color="teal-40" />
+    <Icon type="arrow-fill-right" color="teal-40" />
+    <br />
+    <Icon type="arrow-right" color="teal-40" />
+    <Icon type="dropdown-open" color="teal-40" />
+    <Icon type="plus" color="teal-40" />
+    <Icon type="eye-closed" color="teal-40" />
+    <Icon type="track" color="teal-40" />
+    <Icon type="key" color="teal-40" />
+    <Icon type="arrow-fill-left" color="teal-40" />
+    <br />
+    <Icon type="arrow-top" color="teal-40" />
+    <Icon type="collapse-closed" color="teal-40" />
+    <Icon type="minus" color="teal-40" />
+    <Icon type="lupe" color="teal-40" />
+    <Icon type="calendar" color="teal-40" />
+    <Icon type="upload" color="teal-40" />
+    <Icon type="chevron-right" color="teal-40" />
+    <br />
+    <Icon type="arrow-down" color="teal-40" />
+    <Icon type="collapse-open" color="teal-40" />
+    <Icon type="check" color="teal-40" />
+    <Icon type="trash" color="teal-40" />
+    <Icon type="coin" color="teal-40" />
+    <Icon type="reload" color="teal-40" />
+    <Icon type="chevron-left" color="teal-40" />
+    <br />
+    <Icon type="sort" color="teal-40" />
+    <Icon type="menu-expand" color="teal-40" />
+    <Icon type="menu-collapse" color="teal-40" />
+    <Icon type="menu-default" color="teal-40" />
+    <Icon type="external" color="teal-40" />
+    <Icon type="wallet" color="teal-40" />
+    <Icon type="workspace" color="teal-40" />
 
-  <TextButton text="Default" />
-  <TextButton text="Default" icon="arrow-down" />
+    <h2>Tooltip:</h2>
+    <div class="columns docs-tooltip">
+      <div class="column">
+        <Tooltip id="tooltip-top-start" text="top-start" placement="top-start">
+          <OutlineButton size="large" text="top-start" />
+        </Tooltip>
+      </div>
+      <div class="column">
+        <Tooltip id="tooltip-top" text="top" placement="top">
+          <OutlineButton size="large" text="top" />
+        </Tooltip>
+      </div>
+      <div class="column">
+        <Tooltip id="tooltip-top-end" text="top-end" placement="top-end">
+          <OutlineButton size="large" text="top-end" />
+        </Tooltip>
+      </div>
+    </div>
+    <div class="columns space-between docs-tooltip">
+      <div class="column left">
+        <Tooltip
+          id="tooltip-left-start"
+          text="left-start"
+          placement="left-start">
+          <OutlineButton size="large" text="left-start" />
+        </Tooltip>
+      </div>
+      <div class="column right">
+        <Tooltip
+          id="tooltip-right-start"
+          text="right-start"
+          placement="right-start">
+          <OutlineButton size="large" text="right-start" />
+        </Tooltip>
+      </div>
+    </div>
+    <div class="columns space-between docs-tooltip">
+      <div class="column left">
+        <Tooltip id="tooltip-left" text="left" placement="left">
+          <OutlineButton size="large" text="left" />
+        </Tooltip>
+      </div>
+      <div class="column right">
+        <Tooltip id="tooltip-right" text="right" placement="right">
+          <OutlineButton size="large" text="right" />
+        </Tooltip>
+      </div>
+    </div>
+    <div class="columns space-between docs-tooltip">
+      <div class="column left">
+        <Tooltip id="tooltip-left-end" text="left-end" placement="left-end">
+          <OutlineButton size="large" text="left-end" />
+        </Tooltip>
+      </div>
+      <div class="column right">
+        <Tooltip id="tooltip-right-end" text="right-end" placement="right-end">
+          <OutlineButton size="large" text="right-end" />
+        </Tooltip>
+      </div>
+    </div>
+    <div class="columns docs-tooltip">
+      <div class="column">
+        <Tooltip
+          id="tooltip-bottom-start"
+          text="bottom-start"
+          placement="bottom-start">
+          <OutlineButton size="large" text="bottom-start" />
+        </Tooltip>
+      </div>
+      <div class="column">
+        <Tooltip id="tooltip-bottom" text="bottom" placement="bottom">
+          <OutlineButton size="large" text="bottom" />
+        </Tooltip>
+      </div>
+      <div class="column">
+        <Tooltip
+          id="tooltip-bottom-end"
+          text="bottom-end"
+          placement="bottom-end">
+          <OutlineButton size="large" text="bottom-end" />
+        </Tooltip>
+      </div>
+    </div>
 
-  <h2>TextInput:</h2>
-  <h6>Disabled</h6>
-  <TextInput id="disabled" label="Disabled" disabled />
-  <h6>Default</h6>
-  <TextInput id="id" label="Label" />
-  <h6>Trailing icon</h6>
-  <TextInput
-    id="id2"
-    label="Label"
-    icon="eye-opened"
-    iconColor="mineral-70" />
-  <h6>Prefixed</h6>
-  <TextInput id="prefixed" label="Amount" prefix="R$" />
-  <h6>Suffixed</h6>
-  <TextInput id="duffixed" label="Amount" suffix=".00" />
-  <h6>Required Default</h6>
-  <TextInput id="required" label="Label" helpMessage="Help message" required />
-  <h6>Forced Error - Prop isValid === false</h6>
-  <TextInput
-    id="required2"
-    label="Required"
-    isValid={false}
-    helpMessage="Help message"
-    customErrorMsg="Custom Error Message"
-    required />
-  <h6>Password Input</h6>
-  <PasswordInput
-    id="password-input-component"
-    label="Password Input Component"
-    value="passwordExample" />
+    <h2>Checkbox:</h2>
+    <fieldset>
+      <Checkbox id="checkbox-1" label="Idle" name="checkbox" checked />
+      <br />
+      <Checkbox
+        id="checkbox-2"
+        label="Indeterminate"
+        name="checkbox"
+        indeterminate
+        checked />
+      <br />
+      <Checkbox
+        id="checkbox-3"
+        label="Disabled Checked"
+        name="checkbox"
+        checked
+        disabled />
+      <br />
+      <Checkbox
+        id="checkbox-4"
+        label="Disabled Idle"
+        name="checkbox"
+        disabled />
+    </fieldset>
+
+    <h2>Radio:</h2>
+    <fieldset>
+      <Radio
+        id="radio-1"
+        label="Selected by default"
+        name="radio-group"
+        value="test-value"
+        bind:group={radioButtonsTestValue} />
+      <br />
+      <Radio
+        id="radio-2"
+        label="Idle"
+        name="radio-group"
+        value="test-value-2"
+        bind:group={radioButtonsTestValue} />
+      <br />
+      <Radio
+        id="radio-3"
+        label="Disabled - Checked if first is selected"
+        name="radio-group-2"
+        disabled
+        bind:group={radioButtonsTestValue}
+        value="test-value" />
+    </fieldset>
+
+    <h2>Button:</h2>
+    <Button text="Default" />
+    <Button text="Default" icon="arrow-down" />
+
+    <OutlineButton text="Default" />
+    <OutlineButton text="Default" icon="arrow-down" />
+
+    <TextButton text="Default" />
+    <TextButton text="Default" icon="arrow-down" />
+
+    <h2>TextInput:</h2>
+    <h6>Disabled</h6>
+    <TextInput id="disabled" label="Disabled" disabled />
+    <h6>Default</h6>
+    <TextInput id="id" label="Label" />
+    <h6>Trailing icon</h6>
+    <TextInput
+      id="id2"
+      label="Label"
+      icon="eye-opened"
+      iconColor="mineral-70" />
+    <h6>Prefixed</h6>
+    <TextInput id="prefixed" label="Amount" prefix="R$" />
+    <h6>Suffixed</h6>
+    <TextInput id="duffixed" label="Amount" suffix=".00" />
+    <h6>Required Default</h6>
+    <TextInput
+      id="required"
+      label="Label"
+      helpMessage="Help message"
+      required />
+    <h6>Forced Error - Prop isValid === false</h6>
+    <TextInput
+      id="required2"
+      label="Required"
+      isValid={false}
+      helpMessage="Help message"
+      customErrorMsg="Custom Error Message"
+      required />
+    <h6>Password Input</h6>
+    <PasswordInput
+      id="password-input-component"
+      label="Password Input Component"
+      value="passwordExample" />
+  </div>
 </main>
 
 <style lang="scss" global>
