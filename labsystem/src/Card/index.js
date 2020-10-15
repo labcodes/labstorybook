efc2f529beca88420e723b9e2bcfd7c1021d4e75
Luciano Ratamero <luciano@ratamero.com>
2020-10-15 12:29:23 -0300

    Export OutlineCard as default card component

--- labsystem/src/Card/index.js
+++ labsystem/src/Card/index.js
@@ -1,3 +1,6 @@
+import OutlineCard from "./OutlineCard";
+
+export default OutlineCard;
 export { default as OutlineCard } from "./OutlineCard";
 export { default as FilledCard } from "./FilledCard";
 export { default as OutlineFilledCard } from "./OutlineFilledCard";
