934361a49299227e28adb2abb8e3bfa36a7f3132
Olga Pinheiro <olgapin@gmail.com>
2020-10-21 19:26:46 -0300

    Create index.js to export Sidebar children components using dot notation

---
+++ labsystem/src/Sidebar/index.js
@@ -0,0 +1,20 @@
+import NarrowSidebar from "./Narrow-Sidebar";
+import Header from "./Header";
+import Body from "./Body";
+import Footer from "./Footer";
+import FooterButton from "./FooterButton";
+import UserAvatar from "./UserAvatar";
+import Logotype from "./Logotype";
+import Collapse from "./CollapseButton";
+import Item from "./Item";
+
+NarrowSidebar.Header = Header;
+NarrowSidebar.Body = Body;
+NarrowSidebar.Footer = Footer;
+NarrowSidebar.FooterButton = FooterButton;
+NarrowSidebar.UserAvatar = UserAvatar;
+NarrowSidebar.Logotype = Logotype;
+NarrowSidebar.Collapse = Collapse;
+NarrowSidebar.Item = Item;
+
+export default NarrowSidebar;
