123d24835a590c6a83ccb35ae69be10f1009fc57
cavalcantelucas <lucas@labcodes.com.br>
2020-10-23 20:26:05 -0300

    Add variation selector to 'SystemMessagesPlayground'

--- playgrounds/SystemMessagesPlayground.js
+++ playgrounds/SystemMessagesPlayground.js
@@ -12,25 +12,57 @@ export default class SystemMessagesPlayground extends React.Component {
         Banner,
         Alert,
       },
-      bannerComponent: "Banner",
-      alertComponent: "Alert",
+      currentComponent: "Banner",
     };
   }
 
-  renderComponents = () => {
-    const { availableComponents, bannerComponent, alertComponent, } = this.state;
-    const BannerComponent = availableComponents[bannerComponent];
-    const AlertComponent = availableComponents[alertComponent];
+  renderCurrentComponent = () => {
+    const { availableComponents, currentComponent } = this.state;
+    const Component = availableComponents[currentComponent];
 
     return (
       <>
-        <BannerComponent text="Banner Component" icon="lupe" />
-        <AlertComponent text="Alert Component" icon="wallet" />
+        <h4>
+          <strong>{currentComponent}</strong>
+        </h4>
+        <Component text={`${currentComponent} Component`} icon="lupe" />
       </>
     );
   };
 
+  handleCurrentComponentChange = (e) => {
+    const { value } = e.target;
+    this.setState({
+      currentComponent: value,
+    });
+  };
+
   render() {
-    return <div>{this.renderComponents()}</div>;
+    const { availableComponents } = this.state;
+
+    return (
+      <div className="columns lab-playground">
+        <div className="column lab-plyground__component">
+          {this.renderCurrentComponent()}
+        </div>
+
+        <div className="column lab-playground__configs">
+          <h4>Configurations</h4>
+
+          <span className="lab-playground__item">
+            <label htmlFor="currentComponent">
+              <strong>type: </strong>
+              <select onChange={this.handleCurrentComponentChange}>
+                {Object.keys(availableComponents).map((option) => (
+                  <option key={option} value={option}>
+                    {option}
+                  </option>
+                ))}
+              </select>
+            </label>
+          </span>
+        </div>
+      </div>
+    );
   }
 }
