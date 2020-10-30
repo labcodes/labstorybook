ae66be89831a1e299639c9e13945f06edf35d1a4
Olga Pinheiro <olgapin@gmail.com>
2020-10-30 08:31:38 -0300

    Remove React.Fragment tag because it is not needed

--- labsystem/src/Sidebar/CollapseButton.js
+++ labsystem/src/Sidebar/CollapseButton.js
@@ -4,11 +4,9 @@ import Icon from "../Icon";
 export default class Collapse extends React.Component {
   render() {
     return (
-      <React.Fragment>
-        <button type="button" href="#" className="lab-narrow-sidebar__collapse">
-          <Icon type="menu-collapse" className="sidebar-item" />
-        </button>
-      </React.Fragment>
+      <button type="button" className="lab-narrow-sidebar__collapse">
+        <Icon type="menu-collapse" className="sidebar-item" />
+      </button>
     );
   }
 }
