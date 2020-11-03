e7894d726caee57c13c77c8be150cadc035da3bb
Luciano Ratamero <luciano@ratamero.com>
2020-11-03 16:50:45 -0300

    Refactor components to handle sidebar collapsing

--- labsystem/src/Sidebar/CollapseButton.js
+++ labsystem/src/Sidebar/CollapseButton.js
@@ -1,10 +1,26 @@
 import React from "react";
+import PropTypes from "prop-types";
+
 import Icon from "../Icon";
 
 export default class Collapse extends React.Component {
+  static propTypes = {
+    onClick: PropTypes.func,
+  };
+
+  static defaultProps = {
+    onClick: () => {},
+  };
+
   render() {
+    const { onClick } = this.props;
+
     return (
-      <button type="button" href="#" className="lab-narrow-sidebar__collapse">
+      <button
+        type="button"
+        onClick={onClick}
+        className="lab-narrow-sidebar__collapse"
+      >
         <Icon
           type="menu-collapse"
           className="lab-narrow-sidebar__collapse-icon"
--- labsystem/src/Sidebar/NarrowSidebar.js
+++ labsystem/src/Sidebar/NarrowSidebar.js
@@ -1,44 +1,32 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Icon from "../Icon";
 
 export default class NarrowSidebar extends React.Component {
   static propTypes = {
     children: PropTypes.node.isRequired,
+    isCollapsed: PropTypes.bool,
     isVivid: PropTypes.bool,
     withDividers: PropTypes.bool,
   };
 
   static defaultProps = {
     isVivid: false,
+    isCollapsed: false,
     withDividers: false,
   };
 
   render() {
-    const { children, isVivid, withDividers } = this.props;
+    const { children, isCollapsed, isVivid, withDividers } = this.props;
     return (
-      <React.Fragment>
-        <div className="lab-narrow__overlay" />
-        <div
-          className={
-            "lab-narrow-sidebar__mobile-topbar" +
-            `${isVivid ? " lab-narrow-sidebar--vivid" : ` ""`}`
-          }
-        >
-          <button type="button" className="lab-narrow-sidebar__mobile-button">
-            <Icon type="menu-expand" color="white" />
-          </button>
-        </div>
-        <div
-          className={
-            "lab-narrow-sidebar" +
-            `${isVivid ? " lab-narrow-sidebar--vivid" : ""}` +
-            `${withDividers ? " lab-narrow-sidebar--with-scroll" : ""}`
-          }
-        >
-          {children}
-        </div>
-      </React.Fragment>
+      <div
+        className={`lab-narrow-sidebar
+          ${isVivid ? " lab-narrow-sidebar--vivid" : ""}
+          ${withDividers ? " lab-narrow-sidebar--with-scroll" : ""}
+          ${isCollapsed ? " lab-narrow-sidebar--collapsed" : ""}
+          `}
+      >
+        {children}
+      </div>
     );
   }
 }
