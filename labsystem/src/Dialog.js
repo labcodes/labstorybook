43f0794e49c53770eb5ad747417ae3872a409914
Lucas Cavalcante <lucas@labodes.com.br>
2020-11-05 18:18:35 -0300

    Introduce 'StandardDialog'

--- labsystem/src/Dialog.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default class Dialog extends React.Component {
-  render() {
-    return <h1>Dialog Component</h1>;
-  }
-}
---
+++ labsystem/src/Dialog/index.js
@@ -0,0 +1,1 @@
+export { default as StandardDialog } from "./StandardDialog";
