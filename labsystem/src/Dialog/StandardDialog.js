d4eca6944a50bff25e7db2661e330e51f301410d
Lucas Cavalcante <lucas@labodes.com.br>
2020-11-05 20:44:46 -0300

    Add required props to 'StandardDialog'

--- labsystem/src/Dialog/StandardDialog.js
+++ labsystem/src/Dialog/StandardDialog.js
@@ -1,7 +1,44 @@
 import React from "react";
+import PropTypes from "prop-types";
+import TextButton from "../Buttons/TextButton";
 
 export default class StandardDialog extends React.Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
+    buttonProps: PropTypes.shape({
+      text: PropTypes.string.isRequired,
+      onClick: PropTypes.func.isRequired,
+    }).isRequired,
+  }
+
+  button = () => {
+    const {
+      buttonProps: { text },
+    } = this.props;
+    return <TextButton size="normal" skin="dark" text={text} />;
+  };
+
+  handleClick = (e) => {
+    const {
+      buttonProps: { onClick },
+    } = this.props;
+    onClick(e);
+  };
+
   render() {
-    return <h1>Standard Dialog Component</h1>;
+    const { title, content } = this.props;
+    return (
+      <div className={`lab-dialog lab-dialog--standard`}>
+        <span className="lab-dialog__title">{title}</span>
+        <span className="lab-dialog__content">{content}</span>
+        <span
+          className="lab-dialog__button"
+          onClick={this.handleClick}
+        >
+          {this.button()}
+        </span>
+      </div>
+    );
   }
 }
