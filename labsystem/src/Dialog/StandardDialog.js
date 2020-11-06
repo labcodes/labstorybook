443d2680111d0c52b6321aa35d8f38e293b4fabe
Lucas Cavalcante <lucas@labodes.com.br>
2020-11-06 17:36:12 -0300

    Improve 'StandardDialog' component, and expand its CSS

--- labsystem/src/Dialog/StandardDialog.js
+++ labsystem/src/Dialog/StandardDialog.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import TextButton from "../Buttons/TextButton";
+import { TextButton } from "../Buttons";
 
 export default class StandardDialog extends React.Component {
   static propTypes = {
@@ -10,34 +10,33 @@ export default class StandardDialog extends React.Component {
       text: PropTypes.string.isRequired,
       onClick: PropTypes.func.isRequired,
     }).isRequired,
-  }
-
-  button = () => {
-    const {
-      buttonProps: { text },
-    } = this.props;
-    return <TextButton size="normal" skin="dark" text={text} />;
-  };
-
-  handleClick = (e) => {
-    const {
-      buttonProps: { onClick },
-    } = this.props;
-    onClick(e);
   };
 
   render() {
-    const { title, content } = this.props;
+    const {
+      title,
+      content,
+      buttonProps: { text, onClick },
+    } = this.props;
+
     return (
-      <div className={`lab-dialog lab-dialog--standard`}>
-        <span className="lab-dialog__title">{title}</span>
-        <span className="lab-dialog__content">{content}</span>
-        <span
-          className="lab-dialog__button"
-          onClick={this.handleClick}
-        >
-          {this.button()}
-        </span>
+      <div className="lab-dialog lab-dialog--standard">
+        <div className="lab-dialog__header-wrapper">
+          <div className="lab-dialog__title">{title}</div>
+          <div className="lab-dialog__close-button">close button</div>
+        </div>
+        <p className="lab-dialog__content">{content}</p>
+        <div className="lab-dialog__footer-wrapper">
+          <div className="lab-dialog__optional-button">optional button</div>
+          <div className="lab-dialog__button">
+            <TextButton
+              size="normal"
+              skin="dark"
+              text={text}
+              onClick={onClick}
+            />
+          </div>
+        </div>
       </div>
     );
   }
