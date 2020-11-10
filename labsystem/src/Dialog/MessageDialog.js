71abdd61f6c65f54af9250e1593fdd9b5e8816f3
Lucas Cavalcante <lucas@labodes.com.br>
2020-11-10 20:09:04 -0300

    Improve 'MessageDialog', enable 'title' and 'content' to be editable

--- labsystem/src/Dialog/MessageDialog.js
+++ labsystem/src/Dialog/MessageDialog.js
@@ -3,15 +3,25 @@ import PropTypes from "prop-types";
 import { Button } from "../Buttons";
 
 export default class MessageDialog extends React.Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
+  };
+
   render() {
+    const {
+      title,
+      content,
+    } = this.props;
+
     return (
       <div className="lab-dialog lab-dialog--message">
         <div className="lab-dialog--message__header-wrapper">
           <div className="lab-dialog__close-button--without-background">close button</div>
         </div>
         <div className="lab-dialog--message__icon">icon</div>
-        <div className="lab-dialog--message__title">title</div>
-        <div className="lab-dialog--message__content">content</div>
+        <div className="lab-dialog--message__title">{title}</div>
+        <div className="lab-dialog--message__content">{content}</div>
         <div className="lab-dialog--message__button">
           <Button
             size="normal"
