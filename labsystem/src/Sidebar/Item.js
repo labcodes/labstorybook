f1bb7883762093241ddf4c76e5f4a78426dfd28d
Luciano Ratamero <luciano@ratamero.com>
2020-11-16 16:48:26 -0300

    Refactor Item and add tests

--- labsystem/src/Sidebar/Item.js
+++ labsystem/src/Sidebar/Item.js
@@ -15,40 +15,26 @@ export default class Item extends React.Component {
     isActive: false,
   };
 
-  handleClick = (e) => {
-    const { onClick } = this.props;
-    onClick(e);
-  };
-
-  itemIcon = () => {
-    const { icon } = this.props;
-    return icon ? (
-      <Icon
-        type={icon}
-        color="mineral-60"
-        className="lab-narrow-sidebar__item-icon"
-      />
-    ) : (
-      ""
-    );
-  };
-
   render() {
-    const { label, isActive } = this.props;
+    const { label, icon, isActive, onClick } = this.props;
 
     return (
-      <React.Fragment>
-        <button
-          type="button"
-          className={`lab-narrow-sidebar__item${
-            isActive ? " lab-narrow-sidebar__item--active" : ""
-          }`}
-          onClick={this.handleClick}
-        >
-          {this.itemIcon()}
-          {label}
-        </button>
-      </React.Fragment>
+      <button
+        type="button"
+        className={`lab-narrow-sidebar__item${
+          isActive ? " lab-narrow-sidebar__item--active" : ""
+        }`}
+        onClick={onClick}
+      >
+        {icon ? (
+          <Icon
+            type={icon}
+            color="mineral-60"
+            className="lab-narrow-sidebar__item-icon"
+          />
+        ) : null}
+        {label}
+      </button>
     );
   }
 }
