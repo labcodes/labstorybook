15f669f9c1adbff3a573405264b42ecd01c9d217
Daniela Falcone <danielafalcone@gmail.com>
2020-11-27 09:53:07 -0300

    added lab-checkboxes

--- playgrounds/RadioPlayground.js
+++ playgrounds/RadioPlayground.js
@@ -2,12 +2,15 @@
 import React from "react";
 import Radio from "../labsystem/src/Radio";
 
+import TextInput from "../labsystem/src/Input/TextInput";
+import Checkbox from "../labsystem/src/Checkbox";
+
 export default class RadioPlayground extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      label1: "",
-      label2: "",
+      label1: "Option 1",
+      label2: "Option 2",
       disabled1: false,
       disabled2: false,
       className: undefined,
@@ -50,87 +53,71 @@ export default class RadioPlayground extends React.Component {
       <React.Fragment>
         <div className="columns lab-playground">
           <div className="column lab-playground__component">
-            <h4>Radio</h4>
-            <Radio
-              id="radio-playground-item1"
-              name="radio-playground-group"
-              theme={theme}
-              label={label1}
-              disabled={disabled1}
-              value={label1}
-              className={className}
-              onChange={this.handleChecked}
-            />
-            <br />
-            <Radio
-              id="radio-playground-item2"
-              name="radio-playground-group"
-              theme={theme}
-              label={label2}
-              disabled={disabled2}
-              value={label2}
-              className={className}
-              onChange={this.handleChecked}
-            />
+            <fieldset>
+              <Radio
+                id="radio-playground-item1"
+                name="radio-playground-group"
+                theme={theme}
+                label={label1}
+                disabled={disabled1}
+                value={label1}
+                className={className}
+                onChange={this.handleChecked}
+              />
+              <Radio
+                id="radio-playground-item2"
+                name="radio-playground-group"
+                theme={theme}
+                label={label2}
+                disabled={disabled2}
+                value={label2}
+                className={className}
+                onChange={this.handleChecked}
+              />
+            </fieldset>
           </div>
 
           <div className="column lab-playground__configs">
-            <h4>Configurations</h4>
+            <h3>Prop Settings</h3>
             <span className="lab-playground__item">
-              <label htmlFor="label1">Label 1</label>
-              <br />
-              <input
+              <TextInput
+                label="Option 1 Label"
                 id="label1"
-                type="text"
-                label="label1"
                 value={label1}
                 onChange={this.handleLabel1}
               />
             </span>
             <span className="lab-playground__item">
-              <label htmlFor="label2">Label 2</label>
-              <br />
-              <input
+              <TextInput
+                label="Option 2 Label"
                 id="label2"
-                type="text"
-                label="label2"
                 value={label2}
                 onChange={this.handleLabel2}
               />
             </span>
             <span className="lab-playground__item">
-              <label htmlFor="className">className</label>
-              <br />
-              <input
-                id="className"
-                type="text"
+              <TextInput
                 label="className"
+                id="className"
                 value={className}
                 onChange={this.handleClassName}
               />
             </span>
-            <br />
             <span className="lab-playground__item">
-              <label htmlFor="disabled">Disabled 1</label>
-              <br />
-              <input
-                id="disabled1"
-                type="checkbox"
-                label="disabled1"
-                checked={disabled1}
-                onChange={this.handleDisabled1}
-              />
-            </span>
-            <span className="lab-playground__item">
-              <label htmlFor="disabled">Disabled 2</label>
-              <br />
-              <input
-                id="disabled2"
-                type="checkbox"
-                label="disabled2"
-                checked={disabled2}
-                onChange={this.handleDisabled2}
-              />
+              <fieldset>
+                <Checkbox
+                  id="disabled1"
+                  label="Disable Option 1"
+                  name="disabled"
+                  onChange={this.handleDisabled1}
+                />
+                <Checkbox
+                  id="disabled2"
+                  label="Disable Option 2"
+                  name="checked"
+                  onChange={this.handleDisabled2}
+                />
+              </fieldset>
             </span>
           </div>
         </div>
