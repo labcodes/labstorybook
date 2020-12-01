f7cbe5a0308e02738a83febc0bd74dfb3c653c4c
Luciano Ratamero <luciano@ratamero.com>
2020-12-01 15:44:51 -0300

    Update storybook's configs to use new format

---
+++ .storybook/preview.js
@@ -0,0 +1,6 @@
+import '!style-loader!css-loader!sass-loader!../labsystem/scss/main.scss';
+import '!style-loader!css-loader!sass-loader!../.storybook/scss/main.scss';
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+}
