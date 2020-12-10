d5159f8cbc171dc882a9b3b6f9ceef4cfe28a3eb
Lucas Cavalcante <lucas@labodes.com.br>
2020-12-10 14:49:55 -0300

    Introduce 'ButtonPlayground'

---
+++ playgrounds/ButtonPlayground.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default class ButtonPlayground extends React.Component {
+  render() {
+    return (
+      <div className="columns lab-playground">
+        <div className="column lab-playground__component">
+          <p>Button Playground</p>
+        </div>
+
+        <div className="column lab-playground__configs">
+          <h4>Configurations</h4>
+          <br />
+        </div>
+      </div>
+    );
+  }
+}
