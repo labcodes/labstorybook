05075efc5de147f31ab924489125810f3326655b
Lucas Cavalcante <lucas@labodes.com.br>
2020-12-15 21:26:14 -0300

    Add prop definitions for 'NarrowSidebar'

--- labsystem/src/Sidebar/NarrowSidebar.js
+++ labsystem/src/Sidebar/NarrowSidebar.js
@@ -4,13 +4,13 @@ import PropTypes from "prop-types";
 export const NavbarWithDividersContext = React.createContext(false);
 export default class NarrowSidebar extends React.Component {
   static propTypes = {
-    /** */
+    /** Target component to which the Sidebar should be applied upon. */
     children: PropTypes.node.isRequired,
-    /** */
+    /** In mobile environment, defines if the Sidebar is open. */
     isOpenOnMobile: PropTypes.bool,
-    /** */
+    /** Defines if the rendered Sidebar has a vivid style. */
     isVivid: PropTypes.bool,
-    /** */
+    /** Defines if the Sidebar has dividers. */
     withDividers: PropTypes.bool,
   };
 
