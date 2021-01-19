7f47b9c6b02b3a3a3bcc7de6a484e66e6ccdc3fb
Luciano Ratamero <luciano@ratamero.com>
2021-01-19 13:39:11 -0300

    Fix label positioning on chromium based browsers

--- labsystem/src/Input/AbstractTextInput.js
+++ labsystem/src/Input/AbstractTextInput.js
@@ -205,7 +205,7 @@ export default class AbstractTextInput extends React.Component {
             ref={this.inputRef}
             onChange={this.handleOnChange}
             autoComplete="off"
-            placeholder="" // required for label placement
+            placeholder=" " // required for label placement
             {...(required ? { required } : undefined)}
             {...(disabled ? { disabled } : undefined)}
           />
