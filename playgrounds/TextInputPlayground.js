26de10b6b9786a5e2c2539d5bc18a943fa33dc3a
Daniela Falcone <danielafalcone@gmail.com>
2021-02-10 19:13:25 -0300

    replace iconColor input control for a select in TextInput

--- playgrounds/TextInputPlayground.js
+++ playgrounds/TextInputPlayground.js
@@ -144,6 +144,26 @@ export default class TextInputPlayground extends React.Component {
       "workspace",
     ];
 
+    const iconColorOptions = [
+      "white",
+      "black",
+      "black-75",
+      "mineral-10",
+      "mineral-20",
+      "mineral-30",
+      "mineral-40",
+      "mineral-60",
+      "mineral-70",
+      "mineral-80",
+      "mineral-90",
+      "teal-40",
+      "teal-60",
+      "teal-70",
+      "purple-40",
+      "purple-60",
+      "purple-70",
+    ];
+
     return (
       <div className="columns lab-playground">
         <div className="column lab-playground__component">
@@ -209,14 +229,20 @@ export default class TextInputPlayground extends React.Component {
           <span className="lab-playground__item">
             <label htmlFor="iconColor">
               iconColor
-              <br />
-              <input
+              <select
+                name="iconColor"
                 id="iconColor"
                 type="text"
                 label="iconColor"
                 value={iconColor}
                 onChange={this.handlePropChangeText}
-              />
+              >
+                {iconColorOptions.map((item) => (
+                  <option value={item} key={item}>
+                    {item}
+                  </option>
+                ))}
+              </select>
             </label>
           </span>
           <span className="lab-playground__item">
